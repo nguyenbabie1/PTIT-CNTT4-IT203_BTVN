e327425b75cb6b2d3dd8db25c30c4735103c6c1e
nguyenbabie1 <brokeniuem569@gmail.com>
2026-03-03 08:11:56 +0700

    Move ElectronicProduct class to new package

--- src/Test45min/sessoin16/ElectronicProduct.java
+++ src/Test45min/sessoin16/ElectronicProduct.java
@@ -1,4 +1,4 @@
-package Test45min.sessoin15;
+package Test45min.sessoin16;
 
 public class ElectronicProduct extends Product{
     private int warrantyMonths;
@@ -19,3 +19,4 @@ public class ElectronicProduct extends Product{
         System.out.println("bảo hành"+ warrantyMonths +"tháng");
     }
 }
+
